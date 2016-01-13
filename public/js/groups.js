94ec669d99f58e82ffe5e0c2a7e0882c0b2537b3
em-p <gremleaf@gmail.com>
2016-01-13 14:07:06 -0500

    Update groups.js

--- public/js/groups.js
+++ public/js/groups.js
@@ -1,7 +1,13 @@
 //haxing
 $(document).ready(function(){
-    window.api.get("groups/getGroups/", function(result){
-        console.log("emlynizdumb")
-        //swal("emlynizdumb", result, "success");
-    })
+  window.api.get("groups/getGroups/", function(result){
+    console.log("emlynizdumb")
+  	swal("result", result, "success");
+  })
+  
+  window.api.get("groups/getGroupsIn", function(result){
+    console.log("getting hexed")
+    swal("You are in the groups:", result[0][0]+" "+result[0][1], "success");
+  })
 });
+
